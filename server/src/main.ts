1a79f5ab08351b214e7c40c3282c034ab04475d5
TrevorClute <121002446+TrevorClute@users.noreply.github.com>
2024-11-16 17:59:36 -0500

    d

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,6 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log('ehlp');
-  await app.listen(80, '0.0.0.0');
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
